6ad9115e5e3a6a1d3f9e6197fef43445bb922237
Sawyer Peacock <sawyer@necrosoft.ca>
2019-05-01 17:45:56 -0400

    Basic Code Start Application created without threadding out the file creating process. Think of it like a proof of concept before I massicure this code

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
 
 namespace random_file_generator
 {
@@ -10,6 +12,26 @@ namespace random_file_generator
     {
         static void Main(string[] args)
         {
+            var folderPath = @"C:\TestFolder\";
+            var files2generate = 20;
+
+            for (int i = 0; i < files2generate; i++)
+            {
+                Console.WriteLine("Generating File #{0}", i);
+                GenerateFile(20, folderPath);
+            }
+            Console.WriteLine("Finished Creating Threads");
+            Console.ReadKey();
+        }
+
+        static void GenerateFile(int size, string location)
+        {
+            string filename = String.Format("{0}generated_{1}.txt", location, Guid.NewGuid().ToString());
+            var newFile = File.CreateText(filename);
+            //TODO: Write code to put data in the file
+            Thread.Sleep(1000);
+
+            newFile.Close();
         }
     }
 }
